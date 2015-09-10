3bf0bedffcb1eddc475721a5d39548f04599a657
Marcelo Aliquintuy <mmaliq@gmail.com>
2015-09-10 18:09:50 -0300

    Ejemplo de parse con excepciones

--- src/cl/utfsm/cursojava/clase3/wrappers/NumerosDemo.java
+++ src/cl/utfsm/cursojava/clase3/wrappers/NumerosDemo.java
@@ -2,10 +2,29 @@ package cl.utfsm.cursojava.clase3.wrappers;
 
 public class NumerosDemo {
 	public static void main(String[] args) {
+		Integer pruebaParse;
 		Integer uno = new Integer(1);
 		String strUno = uno.toString();
 		Integer copiaUno = Integer.parseInt(strUno);
-		Integer pruebaParse = Integer.parseInt("46476");
+		try{
+			Float varFloat= Float.parseFloat("1,2");
+		}catch(NumberFormatException e){
+			System.out.println("Existe un Float que no se pude transformar");
+		}
+		
+		try{
+			pruebaParse = Integer.parseInt("asdasd");
+			
+			
+		}catch(NumberFormatException e){
+			System.out.println("Existe un Integer que no se pude transformar");
+		}catch(UnsupportedOperationException e){
+			// nunca se activada, no hay nada que lance esa excepción
+			System.out.println("No se pudo eliminar elemento");
+		}finally {
+			System.out.println("Esto siempre se ejecuta!!");
+		}
+		
 		System.out.println(Integer.MIN_VALUE);
 		System.out.println(Integer.MAX_VALUE);
 		System.out.println(Float.MIN_VALUE);
