3b5b19a283e84038dd649e589fc18ae4c0466c18
Marcelo Aliquintuy <mmaliq@gmail.com>
2015-09-22 17:41:56 -0300

    Se agrega comparacion con miasma referencia act7

--- src/taller1/act7/DemoPuntoEtiquetado.java
+++ src/taller1/act7/DemoPuntoEtiquetado.java
@@ -7,6 +7,7 @@ public class DemoPuntoEtiquetado {
 		PuntoEtiquetado punto2 = new PuntoEtiquetado(2, 3, "a");
 		PuntoEtiquetado punto3 = new PuntoEtiquetado(3, 2, "b");
 		PuntoEtiquetado punto4 = new PuntoEtiquetado(3, 2, "a");
+		PuntoEtiquetado punto5 = punto1;
 		
 		System.out.println("1 eq 2:"+punto1.equals(punto2));
 		System.out.println("1 == 2:"+(punto1.hashCode()== punto2.hashCode()));
@@ -14,6 +15,10 @@ public class DemoPuntoEtiquetado {
 		System.out.println("1 == 3:"+(punto1.hashCode()== punto3.hashCode()));
 		System.out.println("1 eq 4:"+punto1.equals(punto4));
 		System.out.println("1 == 4:"+(punto1.hashCode()== punto4.hashCode()));
+		System.out.println("1 eq 5:"+punto1.equals(punto5));
+		System.out.println("1 == 5:"+(punto1.hashCode()== punto5.hashCode()));
+		
+		
 	}
 
 }
